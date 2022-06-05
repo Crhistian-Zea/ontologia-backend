c8260cfddb019264cca6c4fe7467d8a63eb83a04
Crhistian-Zea <u201512539@upc.edu.pe>
2022-06-05 17:05:06 -0500

    Update code

---
+++ Ontologia.API/Domain/Services/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Ontologia.API.Domain.Models;
+using Ontologia.API.Domain.Services.Communications;
+
+namespace Ontologia.API.Domain.Services
+{
+    public interface IUserService
+    {
+        // General Methods
+        Task<IEnumerable<User>> ListAsync();
+        Task<UserResponse> GetByIdAsync(Guid id);
+        Task<UserResponse> SaveAsync(User user);
+        Task<UserResponse> UpdateAsync(Guid id, User user);
+        Task<UserResponse> DeleteAsync(Guid id);
+
+        // Methods for UserType Entity
+        Task<IEnumerable<User>> ListByUserTypeId(Guid userTypeId);
+        Task<UserResponse> AssignUserToUserType(Guid userTypeId, Guid userId);
+        Task<UserResponse> UnassignUserToUserType(Guid userTypeId, Guid userId);
+    }
+}
